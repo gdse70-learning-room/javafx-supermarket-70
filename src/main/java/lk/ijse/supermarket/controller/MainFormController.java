c272fd09bb46ffe14dde307242ea7d2da49148e9
danujav <danujagreru@gmail.com>
2024-02-16 17:10:29 +0530

    updated main form

--- src/main/java/lk/ijse/supermarket/controller/MainFormController.java
+++ src/main/java/lk/ijse/supermarket/controller/MainFormController.java
@@ -1,8 +1,27 @@
 package lk.ijse.supermarket.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 public class MainFormController {
-    public void btnClickOnAction(ActionEvent actionEvent) {
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) {
+
     }
 }
