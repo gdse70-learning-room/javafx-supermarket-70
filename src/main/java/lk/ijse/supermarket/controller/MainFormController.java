ade4f948cd516e6b2edd7040482fa3515c2013e6
danujav <danujagreru@gmail.com>
2024-02-16 17:42:03 +0530

    finalize the project

--- src/main/java/lk/ijse/supermarket/controller/MainFormController.java
+++ src/main/java/lk/ijse/supermarket/controller/MainFormController.java
@@ -2,26 +2,66 @@ package lk.ijse.supermarket.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainFormController {
     @FXML
-    void btnCustomerOnAction(ActionEvent event) {
+    private AnchorPane root;
+    @FXML
+    private AnchorPane centerNode;
 
+    public void initialize() throws IOException {
+        System.out.println("Main Window just loaded!");
+        loadDashboardForm();
+    }
+
+    private void loadDashboardForm() throws IOException {
+        AnchorPane dashboardPane = FXMLLoader.load(this.getClass().getResource("/view/dashboard_form.fxml"));
+
+        this.centerNode.getChildren().clear();
+        this.centerNode.getChildren().add(dashboardPane);
     }
 
     @FXML
-    void btnDashboardOnAction(ActionEvent event) {
+    void btnCustomerOnAction(ActionEvent event) throws IOException {
+        AnchorPane customerPane = FXMLLoader.load(this.getClass().getResource("/view/customer_form.fxml"));
 
+        this.centerNode.getChildren().clear();
+        this.centerNode.getChildren().add(customerPane);
     }
 
     @FXML
-    void btnItemOnAction(ActionEvent event) {
-
+    void btnDashboardOnAction(ActionEvent event) throws IOException {
+        loadDashboardForm();
     }
 
     @FXML
-    void btnLogoutOnAction(ActionEvent event) {
+    void btnItemOnAction(ActionEvent event) throws IOException {
+        AnchorPane itemPane = FXMLLoader.load(this.getClass().getResource("/view/item_form.fxml"));
 
+        Scene scene = new Scene(itemPane);
+
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("Item Form");
+
+        stage.show();
+    }
+
+    @FXML
+    void btnLogoutOnAction(ActionEvent event) throws IOException {
+        AnchorPane rootNode = FXMLLoader.load(this.getClass().getResource("/view/login_form.fxml"));
+
+        Scene scene = new Scene(rootNode);
+
+        Stage stage = (Stage) this.root.getScene().getWindow();
+        stage.setScene(scene);
+        stage.setTitle("Login Form");
+        stage.centerOnScreen();
     }
 }
